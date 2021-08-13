42106d17311ab56d235a1c2214b0ab4a8cf26697
AxelSotelo <88851084+AxelSotelo@users.noreply.github.com>
2021-08-12 22:10:35 -0500

    Assignment 01 Axel Sotelo

--- assignments/01OperacionesBasicas/src/exercise.py
+++ assignments/01OperacionesBasicas/src/exercise.py
@@ -1,5 +1,28 @@
 def main():
     #escribe tu código abajo de esta línea
+    
+    #this program can solve basic mathematic problems
+
+print("Hello")
+print("Give me a number") #Reads the first number
+num1=int(input())
+print("Give me another number") #Reads the second number
+num2=int(input())
+
+#Solves the calculations
+suma= num1+num2
+sub= num1-num2
+mult= num1*num2
+
+#Shows the answers
+print("The addition of these numbers is ")
+print(suma)
+print("The substraction of these numbers is ")
+print(sub)
+print("The multiplication of these numbers is ")
+print(mult)
+
+print("Goodbye!")
     pass
 
 
