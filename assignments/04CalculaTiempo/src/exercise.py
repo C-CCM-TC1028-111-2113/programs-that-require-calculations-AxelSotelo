edd294a3283beaa2fc4149cd1a5ece6583cf42c4
AxelSotelo <88851084+AxelSotelo@users.noreply.github.com>
2021-08-23 18:57:08 -0500

    Update exercise.py

--- assignments/04CalculaTiempo/src/exercise.py
+++ assignments/04CalculaTiempo/src/exercise.py
@@ -1,8 +1,11 @@
 def main():
     #escribe tu código abajo de esta línea
-    pass
-
-
+    e=int(input("Dame tu edad: "))
+    a=int(input("Dame el año actual: "))
+    f=100-e
+    cien=a+f
+    
+    print("Cumplirás 100 años en el año:", cien)
 
 if __name__ == '__main__':
     main()
