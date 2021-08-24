8067d8a7cabcf6507e7b95789160f10d8ca2830b
AxelSotelo <88851084+AxelSotelo@users.noreply.github.com>
2021-08-23 19:00:41 -0500

    Update exercise.py

--- assignments/05Lustros/src/exercise.py
+++ assignments/05Lustros/src/exercise.py
@@ -1,8 +1,10 @@
 def main():
     #escribe tu código abajo de esta línea
-    pass
-
-
+    n=int(input("Dame el año de nacimiento: "))
+    a=int(input("Dame el año actual: "))
+    e=a-n
+    l=e/5
+    print("Los lustros que has vivido son:",l)
 
 if __name__ == '__main__':
     main()
